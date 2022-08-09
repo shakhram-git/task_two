634a01f55c63eca960da0ecbb7b0867b7b7cc67f
Shakhram <61085162+shakhram-git@users.noreply.github.com>
2022-08-09 21:31:29 +0600

    Toast added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.thedeanda.lorem.LoremIpsum
 
@@ -10,11 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val text = findViewById<TextView>(R.id.text)
         val prefs = getSharedPreferences(PREF_NAME, MODE_PRIVATE)
 
         if (prefs.getInt(COUNT_KEY, 0) > 1) {
-            text.text = LoremIpsum.getInstance().getWords(5, 10)
+            Toast.makeText(this, LoremIpsum.getInstance().getWords(5, 10), Toast.LENGTH_LONG).show()
         }
 
     }
